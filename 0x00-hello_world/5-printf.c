d382d5ca7b4070e7a0083244d603243977efc7f5
root <igwehenry740@>
2022-07-02 13:25:07 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - this is the main
+ *
+ * Description: Print with printf
+ *
+ * Return: return 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
