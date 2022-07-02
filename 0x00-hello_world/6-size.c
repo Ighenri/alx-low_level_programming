d3c28b6a9d64b44d3ab64c964327174a804776f6
root <igwehenry740@>
2022-07-02 15:24:19 -0700

    commit 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - my main entry
+ *
+ * Description: This is to print different size
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char c;
+	int d;
+	long int ld;
+	long long int lld;
+	float f;
+
+	printf("%lu\n", Size of a(char));
+	printf("%lu\n", Size of a(int));
+	printf("%lu\n", Size of a(long int));
+	printf("%lu\n", Size of a(long long int));
+	printf("%lu\n", Size of a(float));
+	return (0);
+}
