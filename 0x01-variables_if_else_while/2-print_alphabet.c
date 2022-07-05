25c159e1946a09bc041e1c7016956de884c66b3d
root <igwehenry740@>
2022-07-04 19:23:13 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - this us my entry
+*Description: Alphabet with putchar
+*Return: 0
+*/
+
+int main(void)
+{
+	char Al;
+
+	for (Al = 'a' ; Al <= 'z' ; Al++)
+	{
+	putchar(Al);
+	}
+	return (0);
+}
