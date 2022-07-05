c01881a4345f4f882f633b630f6bd124f6b00b14
root <igwehenry740@>
2022-07-04 20:54:56 -0700

    omittion of e and q alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main -my entry
+*Description: skip letter q and e in alphabet
+*Return: return 0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+			continue;
+		}
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
