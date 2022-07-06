b549f4f8b958b4b2229863ba6c4092cd98790d4c
root <igwehenry740@>
2022-07-06 16:35:41 -0700

    alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*main - entry point
+*Description: print alphabet in lower case
+*Return: return 0
+*/
+void print_alphabet(void);
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+		_putchar('\n');
+}
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -8,7 +8,7 @@
 void print_alphabet(void);
 
 {
-	_putchar();
+	print_alphabet();
 
 	return (0);
 }
